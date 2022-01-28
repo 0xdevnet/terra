f94d894c3aedad759b246a1fa21a5aa4b15a63a0
Brian Lee <brianlee1597@gmail.com>
2022-01-29 01:18:14 +0900

    refactor websocket

--- Indexer/webSocket.ts
+++ Indexer/webSocket.ts
@@ -1,3 +1,4 @@
+import { MsgExecuteContract } from '@terra-money/terra.js'
 import WebSocket from 'ws'
 import { txDecode } from './decodeLCDTx'
 const ws = new WebSocket("wss://terra-rpc.easy2stake.com/websocket")
@@ -5,8 +6,18 @@ const ws = new WebSocket("wss://terra-rpc.easy2stake.com/websocket")
 //Connect WebSocket
 ws.on('open', () => {
     console.log('Connected to: ' + "wss://terra-rpc.easy2stake.com/websocket")
+    
     //tm.event = "NewBlock" or "Tx" depending on your needs. 
-    ws.send(JSON.stringify({"jsonrpc": "2.0", "method": "subscribe", "id": "1", "params": {"query": "tm.event='Tx'"}}))
+    const rpcParams = {
+        jsonrpc: "2.0", 
+        method: "subscribe", 
+        id: "1", 
+        params: {
+            query: "tm.event='Tx'"
+        }
+    }
+
+    ws.send(JSON.stringify(rpcParams))
 })
 
 //On New Block or Tx
@@ -16,7 +27,14 @@ ws.on('message', (data: WebSocket.RawData) => {
     const decodedTx = txDecode(res?.data?.value?.TxResult?.tx)
 
     // use Tx here
-    console.log(decodedTx?.body.messages)
+    decodedTx?.body.messages.forEach(message => {
+        const readableMessage = JSON.parse(message.toJSON())
+        const typeOfMessage = readableMessage['@type']
+
+        if(typeOfMessage.endsWith("MsgExecuteContract")) {
+            console.log(readableMessage)
+        }
+    })
 })
 
 ws.on('close', () => {
